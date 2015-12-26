b0e1be1ddb070c678483b1a14c62d53f62e47793
MatOchal <mateusz.ochal@yahoo.com>
2015-12-26 22:45:58 +0000

    Some changes

--- ReactionGame.ino
+++ ReactionGame.ino
@@ -1,10 +1,10 @@
-int P1LED = 11;    // variable to store the servo position
-int P2LED = 5;    // variable to store the servo position
-int resultLED = 9;    // variable to store the servo position
-int P1Button = 3;    // variable to store the servo position
-int P2Button = 2;    // variable to store the servo position
+int P1LED = 11;     // Player 1's LED    
+int P2LED = 5;      // Player 2's LED
+int resultLED = 9;  // result/reaction LED
+int P1Button = 3;   // Player 1's button
+int P2Button = 2;   // Player 1's button
 
-int nOfGames = 4;
+int nOfGames = 4;   // Number of games before a round ends
 
 bool pressed;
 
@@ -19,11 +19,12 @@ void setup() {
 
 void loop() {
   int count = 0;
-  lightRound(3);
-  lightsOn();
-  delay(1000);
-  lightsOff();
-  
+
+  // Intro
+  lightsShine(3);
+  lightsBlink(1, 800);
+
+  // Game Start
   for (int i = 0; i < nOfGames; i++){
     delay(random(1000,5000));
     digitalWrite(resultLED, HIGH);
@@ -42,99 +43,88 @@ void loop() {
         delay(1000);
       }
     }
+    
     lightsOff();
     pressed = 0;
   }
-  
-  lightsOn();
-  delay(300);
-  lightsOff();
-  delay(300);
-  lightsOn();
-  delay(300);
-  lightsOff();
-  delay(300);
-  
-  lightRound(3);
-  lightsOn();
-  delay(1000);
-  lightsOff();
-  
-  delay(800);
-  
-  for (int i = count; i > 0; i--){
-    digitalWrite(P1LED, HIGH);
-    delay(100);
-    digitalWrite(P1LED, LOW);
-    delay(800);
-  }
 
-  delay(800);
-  
-  for (int i = nOfGames-count; i > 0; i--){
-    digitalWrite(P2LED, HIGH);
-    delay(100);
-    digitalWrite(P2LED, LOW);
-    delay(800);
-  }
+  // End of Game
+  lightsBlink(1, 300);
+  lightsShine(3);
+  lightsBlink(1, 800);
 
-  lightRound(3);
-  lightsOn();
-  delay(1000);
-  lightsOff();
-  
+  // Show Scores
+  lightBlink(nOfGames-count, P1LED);
+  delay(800);
+  lightBlink(nOfGames-count, P2LED);
+
+  lightsShine(3);
+  lightsBlink(1, 800);
+
+  // Blink for Winner
   if ( nOfGames/count == 2){
-    while (1){
-      digitalWrite(P1LED, HIGH);
-      digitalWrite(P2LED, HIGH);
-      delay(350);
-      digitalWrite(P1LED, LOW);
-      digitalWrite(P2LED, LOW);
-      delay(350);
-    }
+    lightBlink(6, P1LED, nOfGames-count);
+  } else if ( nOfGames/count > 2){
+    lightBlink(6, P2LED);
+  } else if ( nOfGames/count < 2){
+    lightsBlink(6);
   }
-  if ( nOfGames/count > 2){
-    while (1){
-      digitalWrite(P2LED, HIGH);
-      delay(350);
-      digitalWrite(P2LED, LOW);
-      delay(350);
-    }
+}
+
+void lightBlink(int pin, int cycles){
+  lightBlink(pin, cycles, 350);
+}
+
+void lightBlink(int pin, int cycles, int millisec){
+   for (int i = 0; i < cycles; i++) {
+    digitalWrite(pin, HIGH);
+    delay(millisec);
+    digitalWrite(pin, LOW);
+    delay(millisec);
+   }
+}
+
+void lightsBlink(int cycles){
+  lightsBlink(cycles, 300);
+}
+
+void lightsBlink(int cycles, int millisec){
+  for (int i = 0; i < cycles; i++) {
+    lightsOn();
+    delay(millisec);
+    lightsOff();
+    delay(millisec);
   }
-  if ( nOfGames/count < 2){
-    while (1){
-      digitalWrite(P1LED, HIGH);
-      delay(350);
-      digitalWrite(P1LED, LOW);
-      delay(350);
-    }
+}
+
+void lightsShine (int cycles){
+  lightsShine (cycles, 150);
+}
+
+void lightsShine (int cycles, int millisec){
+  for (int i = 0; i < cycles; i++) {
+    delay(millisec);
+    digitalWrite(P1LED, HIGH);
+    digitalWrite(P2LED, LOW);
+    digitalWrite(resultLED, LOW);
+    
+    delay(millisec);
+    digitalWrite(P1LED, LOW);
+    digitalWrite(P2LED, HIGH);
+    digitalWrite(resultLED, LOW);
+  
+    delay(millisec);
+    digitalWrite(P1LED, LOW);
+    digitalWrite(P2LED, LOW);
+    digitalWrite(resultLED, HIGH);
   }
+  digitalWrite(resultLED, LOW);
 }
 
 void lightsOn(){
   digitalWrite(P1LED, HIGH);
   digitalWrite(P2LED, HIGH);
   digitalWrite(resultLED, HIGH);
-}
-
-void lightRound(int rounds){
-  for (int i = 0; i < rounds; i++) {
-    delay(150);
-    digitalWrite(P1LED, HIGH);
-    digitalWrite(P2LED, LOW);
-    digitalWrite(resultLED, LOW);
-    
-    delay(150);
-    digitalWrite(P1LED, LOW);
-    digitalWrite(P2LED, HIGH);
-    digitalWrite(resultLED, LOW);
-  
-    delay(150);
-    digitalWrite(P1LED, LOW);
-    digitalWrite(P2LED, LOW);
-    digitalWrite(resultLED, HIGH);
-  }
-  digitalWrite(resultLED, LOW);
 }
 
 void lightsOff(){
